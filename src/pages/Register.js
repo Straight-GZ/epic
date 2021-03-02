dbfa31de4d966bd155102edb545dcab6f7762291
Straight <gengzhi1993@163.com>
2021-03-02 14:48:35 +0800

    注册页面

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,11 +1,93 @@
 import React from 'react'
+import {Form, Input, Button} from 'antd'
+import styled from 'styled-components'
 
-function Component() {
+const layout = {
+	labelCol: {span: 6},
+	wrapperCol: {span: 18},
+}
+const tailLayout = {
+	wrapperCol: {offset: 6, span: 18},
+}
+const Wrapper = styled.div`
+  max-width: 600px;
+  margin: 30px auto;
+  box-shadow: 2px 2px 4px 0 rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
+  padding: 20px;
+`
+const Title = styled.h1`
+  text-align: center;
+  margin-bottom: 30px;
+`
+
+const Component = () => {
+	const onFinish = (values) => {
+		console.log('Success:', values)
+	}
+	
+	const onFinishFailed = (errorInfo) => {
+		console.log('Failed:', errorInfo)
+	}
+	const validateUsername = (rule, value) => {
+		if (/\W/.test(value)) return Promise.reject('只能是字母下划线')
+		if (value.length < 4 || value.length > 10) return Promise.reject('用户名4~10个字符')
+		return Promise.resolve()
+	}
+	const validateConfirm = ({getFieldValue}) => ({
+		validator(rule, value) {
+			if (getFieldValue('password') === value) {
+				return Promise.resolve()
+			}
+			return Promise.reject('两次密码不一致')
+		}
+	})
 	return (
-		<>
-			<h1>Register</h1>
-		</>
+		<Wrapper>
+			<Title>注册</Title>
+			<Form
+				{...layout}
+				name = "basic"
+				initialValues = {{remember: true}}
+				onFinish = {onFinish}
+				onFinishFailed = {onFinishFailed}
+			>
+				<Form.Item
+					label = "用户名"
+					name = "username"
+					rules = {[{required: true, message: '请输入用户名'},
+						{validator: validateUsername}]}
+				>
+					<Input/>
+				</Form.Item>
+				
+				<Form.Item
+					label = "密码"
+					name = "password"
+					rules = {[
+						{required: true, message: '请输入密码'},
+						{min: 4, message: '最少四个字符'},
+						{min: 10, message: '最多四个字符'}
+					]}
+				>
+					<Input.Password/>
+				</Form.Item>
+				<Form.Item
+					label = "确认密码"
+					name = "confirmPassword"
+					rules = {[{required: true, message: '再次输入密码'},
+						validateConfirm]}
+				>
+					<Input.Password/>
+				</Form.Item>
+				
+				<Form.Item {...tailLayout}>
+					<Button type = "primary" htmlType = "submit">
+						提交
+					</Button>
+				</Form.Item>
+			</Form>
+		</Wrapper>
 	)
 }
-
 export default Component
